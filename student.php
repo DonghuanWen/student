2700c42daff81f94cec896499429e36dbbb192fc
DonghuanWen <1342285933@qq.com>
2020-09-24 21:48:17 +0800

    Add an index pags

--- student.php
+++ student.php
@@ -27,5 +27,30 @@ class student {
 
     function add_grade($grade) {
         $this->grades[] = $grade;
+    } function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
     }
+
+    function add_status($which, $status) {
+
+        $this->status[$which] = $status;
+    }
+
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' (' . $this->average() . ")\n";
+
+        foreach ($this->emails as $which => $what)
+            $result .= $which . ': ' . $what . "\n";
+        $result .= "\n";
+
+        foreach ($this->status as $which => $what)
+            $result .= $which . ': ' . $what . "\n";
+
+        return '<pre>' . $result . '</pre>';
+    }
+
 }
